e5c5ffc4ad8e505bc92700b9f798dfd0c6432e32
Jhonier Rojas <yonier202@gmail.com>
2024-06-26 22:43:03 -0500

    inicializated

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,12 @@
+{{-- directiva para cargar desde otra vista (trear un nav)--}}
+@extends('layouts.app')
+
+
+@section('titulo')
+    Pagina principal
+@endsection
+
+@section('contenido')
+    Lorem ipsum, dolor sit amet consectetur
+    adipisicing elit. Eum rerum cupiditate tempore, tenetur nisi soluta esse assumenda, accusantium aliquid quos, saepe consequatur maiores dolorem sit iure quidem aliquam recusandae repellat
+@endsection
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    // -- todos los archivos .blade.php --//
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
